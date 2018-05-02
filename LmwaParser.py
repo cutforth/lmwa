1b230d9d8f69c683241d27e07c244aeebbe2e738
cutforth <>
2018-05-02 00:28:29 -0600

    First revision using csv module.

--- LmwaParser.py
+++ LmwaParser.py
@@ -11,8 +11,23 @@ import csv
 def convertFileToDict(fhandle):
     for line in fhandle:
         print(line)
+    print("=============DONE============")
+
+Fieldnames = ['Meter Serial Number','Meter Time','Consumption Type',,,,,,,,,'Volume','Volume Units','Operating Time','Operating Time Units']
 
 InputFile = "20180401_DLC_NoWells.csv"
-fileHandle = open(InputFile)
-print(fileHandle)
-convertFileToDict(fileHandle)
+# fileHandle = open(InputFile)
+# print(fileHandle)
+# convertFileToDict(fileHandle)
+#
+# with open(InputFile,'r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
+#
+# print("=============Now DictReader============")
+
+with open(InputFile,'rt') as CsvFileHandle:
+    reader = csv.DictReader(CsvFileHandle,fieldnames=['A','B'],delimiter=';')
+    for row in reader:
+        print(row['A'], row['B'])

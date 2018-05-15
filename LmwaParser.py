d138e1af5c4a6b9791a8a3dc32d36be829395697
cutforth <>
2018-05-15 00:30:43 -0600

    Add Field definitions.

--- LmwaParser.py
+++ LmwaParser.py
@@ -31,9 +31,69 @@ BitField2 = "TBD"
 BitField1 = "TBD"
 BitField0 = "TBD"
 
-Fieldnames = ['Meter Serial Number', 'Meter Reading', 'Consumption Type' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' , 'Volume', 'Volume Units', 'Operating Time', 'Operating Time Units']
+Field1 = 'Meter Serial Number'
+Field2 = 'Date And Time'
+Field3 = 'Consumption Type'
+Field4 = ''
+Field5 = ''
+Field6 = ''
+Field7 = ''
+Field8 = ''
+Field9 = ''
+Field10 = ''
+Field11 = ''
+Field12 = 'Volume'
+Field13 = 'Volume Units'
+Field14 = 'Operating Time'
+Field15 = 'Operating Time Units'
+Field16 = ''
+Field17 = ''
+Field18 = ''
+Field19 = ''
+Field20 = ''
+Field21 = ''
+Field22 = ''
+Field23 = ''
+Field24 = ''
+Field25 = ''
+Field26 = ''
+Field27 = ''
+Field28 = 'WarningRegister'
+Field29 = ''
+Field30 = ''
+Field31 = ''
+Field32 = ''
+Field33 = ''
+Field34 = 'Historical Volume'
+Field35 = 'Historical Volume Units'
+Field36 = ''
+Field37 = ''
+Field38 = ''
+Field39 = ''
+Field40 = ''
+Field84 = 'Revision'
+Fieldnames = [Field1,  Field2,  Field3,  Field4,  Field5,
+              Field6,  Field7,  Field8,  Field9,  Field10,
+              Field11, Field12, Field13, Field14, Field15,
+              Field16, Field17, Field18, Field19, Field20,
+              Field21, Field22, Field23, Field24, Field25,
+              Field26, Field27, Field28, Field29, Field30,
+              Field31, Field32, Field33, Field34, Field35,
+              Field36, Field37, Field38, Field39, Field40,
+              # Field41, Field42, Field43, Field44, Field45,
+              # Field46, Field47, Field48, Field49, Field50,
+              # Field51, Field52, Field53, Field54, Field55,
+              # Field56, Field57, Field58, Field59, Field60,
+              # Field61, Field62, Field63, Field64, Field65,
+              # Field66, Field67, Field68, Field69, Field70,
+              # Field71, Field72, Field73, Field74, Field75,
+              # Field76, Field77, Field78, Field79, Field80,
+              # Field81, Field82, Field83, Field84
+              ]
 
-InputFile = "20180401_DLC_NoWells.csv"
+#InputFile = "20180401_DLC_NoWells.csv"
+InputFile = "2018.04.01 LMWA Readings.csv"
+#InputFile = "2018.05.06 LMWA Readings.csv"
 # fileHandle = open(InputFile)
 # print(fileHandle)
 # convertFileToDict(fileHandle)
@@ -48,4 +108,4 @@ InputFile = "20180401_DLC_NoWells.csv"
 with open(InputFile,'rt') as CsvFileHandle:
     reader = csv.DictReader(CsvFileHandle,fieldnames=Fieldnames,delimiter=';')
     for row in reader:
-        print(row['Meter Serial Number'], row['Meter Reading'], )
+        print(row['Meter Serial Number'], row['Historical Volume'], )

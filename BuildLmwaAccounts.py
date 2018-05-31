4338446d7e8c9da351afdfec5934a9c97ac7e1f7
cutforth <>
2018-05-31 00:28:31 -0600

    clean up old insert commands add createTable command

--- BuildLmwaAccounts.py
+++ BuildLmwaAccounts.py
@@ -8,6 +8,7 @@ import os
 import time
 import sqlite3
 import pdb
+import warnings
 
 # Take input from a CSV file.
 # For test, keep table structure the same, but create bogus data using bogus CSV file.
@@ -45,14 +46,26 @@ def executeSQLCommand(dbConnection, SQLCommand):
     # myDBCursor.execute(SQLCommand)
     dbConnection.cursor().execute(SQLCommand)
 
+def createTable(dbConnection, dbTablename, columnNames, columnTypes):
+    if len(columnNames) != len(columnTypes):
+        warnings.warn('createTable was given name and type vectors of different length')
+    tableDescription = '('
+    for name,type in zip(columnNames,columnTypes):
+        tableDescription = tableDescription + name + ' ' + type + ', '
+    tableDescription = tableDescription[0:-2] + ')'
+    TABLE_CREATE_COMMAND = 'CREATE TABLE ' + mytablename + ' ' + tableDescription
+    executeSQLCommand(dbConnection,TABLE_CREATE_COMMAND)
+    saveDB(dbConnection)
+
 def insertNewRow(dbConnection, dbTablename, columnNames, dataValues):
     SQLCommand = 'INSERT INTO ' + dbTablename + columnNames + ' VALUES ' + dataValues
-    print(SQLCommand)
+    # print(SQLCommand)
     dbConnection.cursor().execute(SQLCommand)
     saveDB(dbConnection)
 
 pdb.set_trace()
 
+# Define the filename and table name
 myfilename = 'LMWA_ACCOUNTS.db'
 mytablename = 'LmwaAccounts'
 
@@ -61,28 +74,14 @@ deleteExistingDB(myfilename,mytablename)
 
 # Create a new database
 createNewDB(myfilename)
-# print(myfilename,' did not exist!')
 myDBConnection = openDB(myfilename)
 
 # Setup the database table(s)
 # FUTURE IMPROVEMENT: The table name, column names, and types could be read in from a configuration file
-LMWA_ACCOUNTS_TABLE_CREATE_COMMAND = 'CREATE TABLE ' + mytablename + ' (account TEXT, serial_number INTEGER, address TEXT, name TEXT, email TEXT, username TEXT, hash TEXT)'
-executeSQLCommand(myDBConnection,LMWA_ACCOUNTS_TABLE_CREATE_COMMAND)
-saveDB(myDBConnection)
+myColNames = ('account', 'serial_number', 'address', 'name', 'email', 'username', 'hash')
+myColTypes = ('TEXT', 'INTEGER', 'TEXT', 'TEXT', 'TEXT', 'TEXT', 'TEXT')
+createTable(myDBConnection, mytablename, myColNames, myColTypes)
+# LMWA_ACCOUNTS_TABLE_CREATE_COMMAND = 'CREATE TABLE ' + mytablename + ' (account TEXT, serial_number INTEGER, address TEXT, name TEXT, email TEXT, username TEXT, hash TEXT)'
 
-# INSERT_CUTFORTH = 'INSERT INTO LmwaAccounts (account, serial_number, address, name, email, username, hash) VALUES ('A65656625', 65656625,'8637 Hollyhock', 'Craig Cutforth', 'craig.cutforth@gmail.com', 'cutforth', 'TBD')'
-# INSERT_CUTFORTH = "'INSERT INTO LmwaAccounts (account, serial_number, address) VALUES (?, ?, ?)',('A65656625', 65656625,'8637 Hollyhock')"
-# executeSQLCommand(myDBConnection,INSERT_CUTFORTH)
-
-# myDBConnection.cursor().execute('INSERT INTO LmwaAccounts (account, serial_number, address) VALUES (?, ?, ?)',('A65656625', 65656625,'8637 Hollyhock'))
 insertNewRow(myDBConnection, mytablename, '(account, serial_number, address)', '(\'A65656625\', 65656625,\'8637 Hollyhock\')')
 insertNewRow(myDBConnection, mytablename, '(account, serial_number, address)', '(\'A65656648\', 65656648,\'8632 Hollyhock\')')
-
-# LMWA_ACCOUNTS_ADD_HARRIS_INFO = '(\'A65656648\', 65656648, \'8632 Hollyhock\')'
-# INSERT_HARRIS = 'INSERT INTO ' + mytablename + ' (account, serial_number, address) VALUES (?, ?, ?)\', ' + LMWA_ACCOUNTS_ADD_HARRIS_INFO
-# pdb.set_trace()
-# executeSQLCommand(myDBConnection,INSERT_HARRIS)
-# myDBConnection.cursor().execute('INSERT INTO LmwaAccounts (account, serial_number, address) VALUES (?, ?, ?)',('A65656648', 65656648,'8632 Hollyhock'))
-# saveDB(myDBConnection)
-
-# executeSQLCommand(myDBConnection)
